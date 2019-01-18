e859e195aad89ff819ad8bb1f8d5bf7b55599a65
Yuuki Izumi <izumi-y@im00.cs.uec.ac.jp>
2019-01-18 11:55:51 +0900

    0.5にできてなかったのを修正

--- make_conv0.py
+++ make_conv0.py
@@ -3,41 +3,37 @@ import numpy as np
 import os
 
 
-param_path = "make_file/conv02to01"
+param_path = "make_file/conv02to01v4"
 
 if not os.path.isdir(param_path):
     os.makedirs(param_path)
 
-# w = np.arange(9).reshape((3, 1, 1, 3))
+w = np.arange(9).reshape((3, 1, 1, 3))
 # w = np.arange(208).reshape((16, 1, 1, 13))
-w = np.arange(16).reshape((4, 1, 1, 4))
+# w = np.arange(16).reshape((4, 1, 1, 4))
 b = [-120.0, -120.0, -120.0]
 # b = [-1.0, -1.0, -1.0, 0.0]
 
-
-
+w = np.array(w, dtype="float32")
 
 w[0][0][0][0] = 0.5
 w[0][0][0][1] = 0.0
 w[0][0][0][2] = 0.0
-w[0][0][0][3] = 0.0 
+# w[0][0][0][3] = 0.0
 w[1][0][0][0] = 0.0
 w[1][0][0][1] = 0.5
 w[1][0][0][2] = 0.0
-w[1][0][0][3] = 0.0
+# w[1][0][0][3] = 0.0
 w[2][0][0][0] = 0.0
 w[2][0][0][1] = 0.0
 w[2][0][0][2] = 0.5
-w[2][0][0][3] = 0.0
-w[3][0][0][0] = 0.0
-w[3][0][0][1] = 0.0
-w[3][0][0][2] = 0.0
-w[3][0][0][3] = 1.0
+# w[2][0][0][3] = 0.0
+# w[3][0][0][0] = 0.0
+# w[3][0][0][1] = 0.0
+# w[3][0][0][2] = 0.0
+# w[3][0][0][3] = 1.0
 
-
-
-
-
+print(w)
 
 
 w2 = np.array(w, dtype="float32")
