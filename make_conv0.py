ce29b6f255eb92d69a7ff59d074135f4adc60d16
izumi <izumi@yanailab-no-MacBook-Air.local>
2019-02-08 18:25:35 +0900

    make convW,convb for background mask

--- make_conv0.py
+++ make_conv0.py
@@ -3,35 +3,38 @@ import numpy as np
 import os
 
 
-param_path = "make_file/conv02to01v5"
+param_path = "make_file/backgroundmask"
+
+# backgroundmaskは−１のフィルタかけて、＋１
 
 if not os.path.isdir(param_path):
     os.makedirs(param_path)
 
-# w = np.arange(9).reshape((3, 1, 1, 3))
+w = np.arange(9).reshape((3, 1, 1, 3))
 # w = np.arange(208).reshape((16, 1, 1, 13))
-w = np.arange(16).reshape((4, 1, 1, 4))
-b = [-120.0, -120.0, -120.0]
+# w = np.arange(16).reshape((4, 1, 1, 4))
+# b = [-120.0, -120.0, -120.0]
 # b = [-1.0, -1.0, -1.0, 0.0]
+b = [1.0, 1.0, 1.0]
 
 w = np.array(w, dtype="float32")
 
-w[0][0][0][0] = 0.5
+w[0][0][0][0] = -1.0
 w[0][0][0][1] = 0.0
 w[0][0][0][2] = 0.0
-w[0][0][0][3] = 0.0
+# w[0][0][0][3] = 0.0
 w[1][0][0][0] = 0.0
-w[1][0][0][1] = 0.5
+w[1][0][0][1] = -1.0
 w[1][0][0][2] = 0.0
-w[1][0][0][3] = 0.0
+# w[1][0][0][3] = 0.0
 w[2][0][0][0] = 0.0
 w[2][0][0][1] = 0.0
-w[2][0][0][2] = 0.5
-w[2][0][0][3] = 0.0
-w[3][0][0][0] = 0.0
-w[3][0][0][1] = 0.0
-w[3][0][0][2] = 0.0
-w[3][0][0][3] = 0.5
+w[2][0][0][2] = -1.0
+# w[2][0][0][3] = 0.0
+# w[3][0][0][0] = 0.0
+# w[3][0][0][1] = 0.0
+# w[3][0][0][2] = 0.0
+# w[3][0][0][3] = 0.5
 
 print(w)
 
@@ -44,5 +47,5 @@ print(b2.shape)
 
 if not os.path.isdir(param_path):
     os.mkdir(param_path)
-np.array(w2).tofile(os.path.join(param_path, "conv02to01_w" + ".bin"))
-# np.array(b2).tofile(os.path.join(param_path, "conv00a_b" + ".bin"))
+np.array(w2).tofile(os.path.join(param_path, "bmask_w" + ".bin"))
+np.array(b2).tofile(os.path.join(param_path, "bmask_b" + ".bin"))
